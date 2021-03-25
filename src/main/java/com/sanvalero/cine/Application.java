ab7f8090600eeca6f6072485b0f8fcfbcf96ca9a
David de San Lázaro <david.dslazaro@gmail.com>
2021-03-25 16:36:29 +0100

    Creación del proyecto

---
+++ src/main/java/com/sanvalero/cine/Application.java
@@ -0,0 +1,15 @@
+
+/* DAVID DE SAN LÁZARO LORENTE
+ * ACTIVIDAD DE APRENDIZAJE SEGUNDA EVALUACIÓN
+ *
+ * Esta es una aplicación para gestionar una cadena de cines. Su función es introducir la información de sus
+ * salas, películas o trabajadores. También permite consultar la información introducida.
+ */
+
+package com.sanvalero.cine;
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
